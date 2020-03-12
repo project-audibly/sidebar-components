24ade29b3c119653b9dc9b764fb0381e848c4158
Joanna Wang <shiju.an362@gmail.com>
2020-03-12 11:59:02 -0700

    connect server

--- server/index.js
+++ server/index.js
@@ -1,4 +1,10 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+const port = 3003;
 
-// app.use(express.static(''));
+app.use(express.static('dist'));
+
+app.use(bodyParser.json());
+
+app.listen(port, () => console.log(`DO YOU COME IN @ ${port}`));
