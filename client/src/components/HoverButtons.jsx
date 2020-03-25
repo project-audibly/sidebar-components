9499095db886ee88a30ac0b26437fad9734783bf
Joanna Wang <shiju.an362@gmail.com>
2020-03-25 15:42:14 -0700

    adjust on mouseleave status depending on isclicked state

--- client/src/components/HoverButtons.jsx
+++ client/src/components/HoverButtons.jsx
@@ -12,18 +12,12 @@ class HoverButtons extends React.Component {
       isVisible: false,
       isPlaying: false,
       isClicked: false,
-      isShowing: false,
     }
-    this.toggleOptions = this.toggleOptions.bind(this);
     this.handleMouseOver = this.handleMouseOver.bind(this);
     this.handleMouseExit = this.handleMouseExit.bind(this);
     this.playSong = this.playSong.bind(this);
     this.pauseSong = this.pauseSong.bind(this);
     this.stayVisible = this.stayVisible.bind(this);
-  }
-
-  toggleOptions() {
-    this.setState({ isShowing: !this.state.isShowing });
   }
 
   handleMouseOver() {
@@ -36,18 +30,16 @@ class HoverButtons extends React.Component {
 
   playSong() {
     this.setState({ isPlaying: true });
+    this.setState({ isClicked: true})
   }
 
   pauseSong() {
     this.setState({ isPlaying: false });
+    this.setState({ isClicked: false})
   }
 
   stayVisible() {
     this.setState({ isClicked: !this.state.isClicked });
-  }
-
-  componentWillUnmount() {
-    window.removeEventListener("mouseleave");
   }
 
   render() {
@@ -62,22 +54,21 @@ class HoverButtons extends React.Component {
     }
 
     return (
-      <div>
-
+      <div className="hoverOverlay">
         {this.state.isVisible ?
-          (<div className="hoverOverlay" onMouseLeave={this.handleMouseExit}>
+          (
+          <div className="hoverArea" onMouseLeave={ this.state.isClicked ? () => {} : this.handleMouseExit }>
             <div className="hoverContainer">
               <div className="mainHoverBtn">
                 {button}
               </div>
               <div className="optionalHoverBtn">
                 <button className="optionalBtn" onClick={this.stayVisible}><img src="https://audibly-sb-media.s3-us-west-1.amazonaws.com/icons/heart.png" className="optionalIcon" /></button>
-                {/* <MoreButton onClick={this.stayVisible}/> */}
-                <button className="optionalBtn" onClick={() => { this.toggleOptions(); this.stayVisible() }}
-                ><img src="https://audibly-sb-media.s3-us-west-1.amazonaws.com/icons/more.png" className="optionalIcon" /><MoreButton isShowing={this.state.isShowing} /></button>
+                <MoreButton onClick={this.stayVisible}/>
               </div>
             </div>
-          </div>)
+          </div>
+          )
           :
           (null)
         }
--- client/src/components/MoreButton.jsx
+++ client/src/components/MoreButton.jsx
@@ -4,27 +4,29 @@ import styles from './MoreButton.css';
 class MoreButton extends React.Component {
   constructor(props) {
     super(props)
-    // this.state = {
-    //   isShowing: false,
-    // }
-  //   this.toggleOptions = this.toggleOptions.bind(this);
-  // }
+    this.state = {
+      isShowing: false,
+    }
+    this.toggleOptions = this.toggleOptions.bind(this);
+  }
 
-  // toggleOptions() {
-  //   this.setState({ isShowing: !this.state.isShowing });
+  toggleOptions() {
+    this.setState({ isShowing: !this.state.isShowing });
   }
 
   render() {
     return (
       <div>
-        {/* <button className="optionalBtn"
-          onClick={ this.toggleOptions }
-          >
+        <button className="optionalBtn"
+          onClick={ () => {
+            this.toggleOptions();
+            this.props.onClick(); 
+            }
+          }>
           <img src="https://audibly-sb-media.s3-us-west-1.amazonaws.com/icons/more.png" className="optionalIcon" />
         </button>
- */}
 
-        {this.props.isShowing ? 
+        {this.state.isShowing ? 
           (
             <div className="moreOptions">
               <div>
@@ -42,7 +44,8 @@ class MoreButton extends React.Component {
                 <button className="moreOptionsBtn">Station</button>
               </div>
             </div>
-          ) : 
+          ) 
+          : 
           ( null)
         }
 
